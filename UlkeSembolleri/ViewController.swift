06562200ebcbef632c6d75e3b46a2f1d3d24dcb9
KAAN YİĞİT <>
2021-04-09 02:25:13 +0300

    saat 2.25 de değişiklik yaptım.

--- UlkeSembolleri/ViewController.swift
+++ UlkeSembolleri/ViewController.swift
@@ -7,9 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "k1"
+        return cell
+    }
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
