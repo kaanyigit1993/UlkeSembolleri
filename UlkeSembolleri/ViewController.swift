bb2adefc8e8279e0b0111fc153b3d222c45da114
KAAN YİĞİT <>
2021-04-09 02:23:02 +0300

    first commit

---
+++ UlkeSembolleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UlkeSembolleri
+//
+//  Created by KAAN YİĞİT on 9.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
